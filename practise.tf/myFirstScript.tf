afc40660c31606ccacae9c7e908ce9823aee40af
shweta <bawaneshweta1994@gmail.com>
2023-05-05 18:25:43 +0530

    user added

--- practise.tf/myFirstScript.tf
+++ practise.tf/myFirstScript.tf
@@ -1,10 +1,10 @@
-provider "aws" {
+/*provider "aws" {
   region = "us-east-2"
-}
+}*/
 
-resource "aws_instance" "web-server" {
+/*resource "aws_instance" "web-server" {
   ami = "ami-08333bccc35d71140"
   security_groups = [ "k8s-sg" ]
   key_name = "ohio-key"
   instance_type = "t2.micro"
-}
+}*/
